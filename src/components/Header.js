edc9c6bd7a465ca87ad06b928932f99ae315c624
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-11 00:27:29 -0600

    Add menu button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import MenuButton from './MenuButton';
 import '../styles/header.css';
 
 const Header = () => (
   <header>
+    <MenuButton />
     <a href="https://www.google.com">About</a>
     <a href="https://www.google.com">Projects</a>
     <a href="https://www.google.com">Github</a>
---
+++ src/components/MenuButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import menu from './assets/menu36px.png';
+
+const MenuButton = () => (
+  <img src={menu} alt="Menu button" />
+);
+
+export default MenuButton;
