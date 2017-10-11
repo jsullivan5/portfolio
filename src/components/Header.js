af53d663fb1477b79db0084713806c56e798635c
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-11 16:51:05 -0600

    Add header transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../styles/header.css';
 
-const Header = ({ menu }) => (
-  <header className={typeof menu === 'boolean' && menu ? 'visible' : 'hidden'}>
-    <div />
-    <a href="https://www.google.com">About</a>
-    <a href="https://www.google.com">Projects</a>
-    <a href="https://www.google.com">Github</a>
-    <a href="https://www.google.com">LinkedIn</a>
-  </header>
-);
+const Header = ({ menu }) => {
+  if (menu === '') {
+    return <div />;
+  }
+  return (
+    <header className={menu !== '' && menu === true ? 'visible' : 'hidden'}>
+      <div />
+      <a href="https://www.google.com">About</a>
+      <a href="https://www.google.com">Projects</a>
+      <a href="https://www.google.com">Github</a>
+      <a href="https://www.google.com">LinkedIn</a>
+    </header>
+  );
+};
 
 export default Header;
