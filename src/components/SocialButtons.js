30b5fb4b410032e679f64e9b2c87676bc461b9b5
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-13 13:18:00 -0600

    Add social icons

---
+++ src/components/SocialButtons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialButtons = () => (
+  <div>
+    <img src="" alt="Github link" />
+    <img src="" alt="LinkedIn link" />
+    <img src="" alt="Resume link" />
+  </div>
+);
+
+export default SocialButtons;
