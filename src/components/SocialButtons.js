8d518caf53483e34a364bc3953b2a0bb51495a17
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-13 16:36:24 -0600

    Add scale transition to social icons

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import '../styles/SocialButtons.css';
 import linkedin from './assets/linkedin.png';
-import github from './assets/github.png';
+import github from './assets/github-character.png';
 import resume from './assets/resume.png';
 
 const SocialButtons = () => (
   <div className="social-container">
     <a href="https://github.com/jsullivan5">
-      <img className="github-link" src={github} alt="Github link" />
+      <img
+        className="github-link icon-pop"
+        src={github}
+        alt="Github link"
+        title="github"
+      />
     </a>
     <a href="https://www.linkedin.com/in/james-f-sullivan/">
-      <img className="resume-link" src={resume} alt="Resume link" />
+      <img
+        className="linkedin-link icon-pop"
+        src={linkedin}
+        alt="LinkedIn link"
+      />
     </a>
     <a href="https://www.linkedin.com/in/james-f-sullivan/">
-      <img className="linkedin-link" src={linkedin} alt="LinkedIn link" />
+      <img
+        className="resume-link icon-pop"
+        src={resume}
+        alt="Resume link"
+      />
     </a>
   </div>
 );
